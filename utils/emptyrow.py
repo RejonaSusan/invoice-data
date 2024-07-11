19422113f885f693eca89e81d84b1892141100ee
Susan <rejona.susan-ext@hpe.com>
2024-07-11 14:20:04 +0530

    a

--- utils/emptyrow.py
+++ utils/emptyrow.py
@@ -1,26 +1,26 @@
+import openpyxl
 
-def find_empty_row(table, headers):
-    def is_row_empty(table, row_index):
-        for col_index in range(table.num_cols):
-            cell = table.cell(row_index, col_index)
-            if cell and cell.value:
+def find_empty_row(sheet, headers):
+    def is_row_empty(sheet, row_index):
+        for col_index in range(1, sheet.max_column + 1):
+            cell_value = sheet.cell(row=row_index, column=col_index).value
+            if cell_value:
                 return False
         return True
 
-    rows = table.rows()
     empty_row = None
 
-    for i in range(len(rows)):
-        if is_row_empty(table, i):
+    for i in range(1, sheet.max_row + 1):
+        if is_row_empty(sheet, i):
             empty_row = i
             break
 
     if empty_row is None:
-        empty_row = table.num_rows
+        empty_row = sheet.max_row + 1
 
-    if empty_row == 0:
+    if empty_row == 1:
         for col_num, header in enumerate(headers):
-            table.write(0, col_num, header)
+            sheet.cell(row=1, column=col_num + 1, value=header)
         empty_row += 1
 
     return empty_row
--- utils/writeinv.py
+++ utils/writeinv.py
@@ -7,12 +7,14 @@ config.read(config_path)
 
 INVOICE_FIELDS = config['InvoiceFields']['fields'].split(',')
 
-def write_invoices(invoices_data, table, empty_row):
+def write_invoices(invoices_data, sheet, empty_row):
     for idx, invoice in enumerate(invoices_data):
         row = []
         for field in INVOICE_FIELDS:
             field_value = invoice.fields.get(field)
             row.append(field_value.value if field_value else "")
         for col_num, value in enumerate(row):
-            table.write(empty_row, col_num, str(value))
+            sheet.cell(row=empty_row, column=col_num + 1, value=str(value))
         empty_row += 1
+
+
